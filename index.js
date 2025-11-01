53bb8a993abe197ba6d1a000c2f608d7f6cde1c5
Andres Santamarina <andresesantamarina@gmail.com>
2025-11-01 16:15:04 -0300

    cors

--- index.js
+++ index.js
@@ -84,8 +84,10 @@ io.use(async (socket, next) => {
 
 initSocketIO(io);
 
-httpServer.listen(port, () => {
-  console.log('Servidor corriendo en el puerto ' + port);
-});
+// httpServer.listen(port, () => {
+//   console.log('Servidor corriendo en el puerto ' + port);
+// });
 
 export { io };
+
+export default httpServer
